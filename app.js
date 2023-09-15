5e5cd065fec70e9243db6bd2d02f3ddaf02b95af
MIKE FOSTER <HELLOMIKEFOSTER@GMAIL.COM>
2023-09-15 14:52:29 +0100

    Add FOR and WHILE loops

--- app.js
+++ app.js
@@ -8,17 +8,25 @@ let message;
 
 if (rateYourself <= 4) {
 message = "Welcome! I shall teach you to dance.";
-} else if (rateYourself <=7) {
+} else if (rateYourself <= 7) {
     message = "You are a competent dancer, but I have more to teach you."
-} else if (rateYourself <=10) {
+} else if (rateYourself <= 10) {
     message = "YOU KNOW TOO MUCH. I CAN TEACH YOU NOTHING. LEAVE."
-} else {
-    message = "If you're not going to do this properly, I'm not going to bother with you."
 }
 
-message = "You have rated yourself as having a dance ability of " + rateYourself + " out of 10. " + message;
+while (rateYourself < 1 || rateYourself > 10) {
+    rateYourself = prompt("Do it properly or I'm calling the police. Rate your dance ability on a scale of 1-10.")
+}
+
+for (let i = 0; i < rateYourself; i++) {
+    document.write("<img src='ghoulies2.png'/>")
+    }
+
+message = "<br>You have rated yourself as having a dance ability of " + rateYourself + " out of 10. " + message;
 document.write(message)
 }
+
+
 
 
 function dontLeaveMe() {
